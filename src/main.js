00c58680263b890d1ac528e1f9825b2f4f12c607
sergioatk <55900410+sergioatk@users.noreply.github.com>
2021-04-16 00:29:24 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,22 @@
 let pagActual = 00;
+armarListado(pagActual);
+
+document.querySelector('.nav').onclick = function(e) {
+    
+    if (e.target.textContent.toLowerCase() === 'siguiente') {
+        manejarNav(true);
+    } else if (e.target.textContent.toLowerCase() === 'atras') {
+        manejarNav(false);
+}
+}
+
+
+document.querySelector('#listado').onclick = function(e) {
+    if (e.target.classList.contains('poke-listado')) {
+        mostrarInfo(e.target);
+    }
+}
+
 
 
 function buscarEnAPI(offset = 00) {
@@ -42,19 +60,28 @@ function armarListado(offset) {
 
         listado.forEach(function(pokemon) {
             
-            
+            const nuevoItem = document.createElement('div');
+
             const nuevoCuadro = document.createElement('div');
-            nuevoCuadro.classList.add('col-xl-3');
-            nuevoCuadro.classList.add('col-xs-2');
-            nuevoCuadro.classList.add('col-lg-4');
-            nuevoCuadro.classList.add('col-md-6');
-            nuevoCuadro.classList.add('poke-listado');
+            nuevoCuadro.id = pokemon.name;
+            
+            nuevoItem.classList.add('col-xl-3');
+            nuevoItem.classList.add('col-xs-2');
+            nuevoItem.classList.add('col-lg-4');
+            nuevoItem.classList.add('col-md-6');
+            nuevoItem.classList.add('poke-listado');
+            nuevoItem.id = pokemon.name;
+            //console.log(nuevoItem.id)
             nuevoCuadro.textContent = pokemon.name;
+            nuevoCuadro.classList.add('poke-listado');
 
             const nuevaImagen = document.createElement('img');
             buscarEnAPI(pokemon.name).then(resultado => {
-                //console.log(resultado.sprites.front_default);
+                //console.log(resultado.abilities[0].ability.name);
                 nuevaImagen.src = resultado.sprites.front_default
+                nuevaImagen.classList.add('poke-listado');
+                nuevaImagen.id = pokemon.name;
+                //console.log(nuevaImagen.id)
                 
             })
 
@@ -64,21 +91,36 @@ function armarListado(offset) {
 
 
 
-            $listado.appendChild(nuevoCuadro);
-            $listado.appendChild(nuevaImagen);
+            nuevoItem.appendChild(nuevoCuadro);
+            nuevoItem.appendChild(nuevaImagen);
+
+            $listado.appendChild(nuevoItem);
 
         })
     })
 }
 
-document.querySelector('.nav').onclick = function(e) {
-    
-    if (e.target.textContent.toLowerCase() === 'siguiente') {
-        manejarNav(true);
-    } else if (e.target.textContent.toLowerCase() === 'atras') {
-        manejarNav(false);
+
+
+
+function mostrarInfo(pokemon) {
+    const $nombre = document.querySelector('#nombre');
+    const $altura = document.querySelector('#altura');
+    const $peso = document.querySelector('#peso');
+    const $habilidad = document.querySelector('#habilidad');
+
+
+    buscarEnAPI(pokemon.id).then(respuesta => {
+        
+
+        $nombre.textContent = respuesta.name;
+        $altura.textContent = respuesta.height;
+        $peso.textContent = respuesta.weight;
+        $habilidad.textContent = respuesta.abilities[0].ability.name;
+    })
 }
-}
+
+
 
 
 function manejarNav(direccion) {
@@ -103,4 +145,3 @@ function manejarNav(direccion) {
 }
 
 
-armarListado(pagActual);
