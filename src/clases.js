f8efa776977f7d7bdf3622cc0e6065e9d7838d2b
sergioatk <55900410+sergioatk@users.noreply.github.com>
2021-05-02 10:07:20 -0300

    corrige clase

--- src/clases.js
+++ src/clases.js
@@ -1,8 +1,9 @@
 export class Pokemon {
-    constructor(nombre, peso, altura, habilidades) {
+    constructor(nombre, peso, altura, habilidades, imagen) {
         this.nombre = nombre;
         this.peso = peso;
         this.altura = altura;
         this.habilidades = habilidades;
+        this.imagen = imagen;
     }
 }
