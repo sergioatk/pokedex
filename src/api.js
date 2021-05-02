ee178b70fc08f833388234cef32fc6432b498123
sergioatk <55900410+sergioatk@users.noreply.github.com>
2021-05-02 09:02:44 -0300

    emprolija api

--- src/api.js
+++ src/api.js
@@ -1,12 +1,22 @@
+import { Pokemon } from "./clases.js";
+
 export async function pedirDatos(parametro = 0) {
+    const BASE_URL = 'https://pokeapi.co/api/v2/pokemon/'
     if (typeof parametro === 'number') {
-      const respuestaAPI = await fetch(`https://pokeapi.co/api/v2/pokemon/?offset=${parametro}&limit=20`);
+      const respuestaAPI = await fetch(`${BASE_URL}?offset=${parametro}&limit=20`);
       const respuestaJSON = respuestaAPI.json();
+      
       return respuestaJSON;
     } else {
-    const respuestaAPI = await fetch(`https://pokeapi.co/api/v2/pokemon/${parametro}`);
+    const respuestaAPI = await fetch(`${BASE_URL}${parametro}`);
     const respuestaJSON = respuestaAPI.json(); 
+
+    
+    //console.log(respuestaJSON);
+    //console.log(nuevoPokemon);
+
     return respuestaJSON;
+    
     }
   }
   
