599d53ef6ae7f3e555672697b7c10ba68f623333
sergioatk <55900410+sergioatk@users.noreply.github.com>
2021-05-02 10:07:51 -0300

    Update input.js

--- src/input.js
+++ src/input.js
@@ -1,3 +1,6 @@
+import { Pokemon } from "./clases.js";
+
+
 export let offset = 0;
 export async function manejarInputLista(callbackAPI, callbackMostrarInfo) {
     const lista = document.querySelector('.list-group');
@@ -6,7 +9,8 @@ export async function manejarInputLista(callbackAPI, callbackMostrarInfo) {
       if (elemento.classList.contains('list-group-item')) {
         const infoPokemon = callbackAPI(elemento.dataset.nombre)
           .then((info) => {
-            callbackMostrarInfo(info);
+            const nuevoPokemon = new Pokemon(info.name, info.weight, info.height, info.abilities, info.sprites.front_default);
+            callbackMostrarInfo(nuevoPokemon);
           })
       }
     }
